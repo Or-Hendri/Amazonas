42f31a80acd8328ea3f06ccb0220595b21aed2dc
Or Hendri <orior12345677@gmail.com>
2023-10-02 02:37:25 +0300

    Theme provider

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import cors from 'cors'
 import express, { Request, Response } from 'express'
 import { sampleProducts } from './data'
-
 const app = express()
 app.use(
   cors({
@@ -9,10 +8,14 @@ app.use(
     origin: ['http://localhost:5173'],
   })
 )
-
 app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProducts)
 })
+
+app.get('/api/products/:slug', (req: Request, res: Response) => {
+  res.json(sampleProducts.find((x) => x.slug === req.params.slug))
+})
+
 const PORT = 4000
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`)
